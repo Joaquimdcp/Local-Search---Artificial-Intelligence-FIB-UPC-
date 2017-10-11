1684b5ffc3a2cc22a961b3bea06aa6443c7bbdc6
Joaquim <joaquimdcp@gmail.com>
2017-10-11 11:22:45 +0200

    New class State

---
+++ src/com/company/Peticio.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Peticio {
+    public int X;
+    public int Y;
+    public int dies;
+
+    public Peticio(int X, int Y, int dies) {
+        System.out.println(X + " " + Y + " "+ dies);
+        this.X = X;
+        this.Y = Y;
+        this.dies = dies;
+    }
+}
+
