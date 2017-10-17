b519fb3f91073bc8d76d70075651e3b78b5c5182
marti.ferret <marti.ferret@est.fib.upc.edu>
2017-10-17 19:03:58 +0200

    Esquema de funcio de distribucio

--- src/com/company/DistributionSuccessorFunction.java
+++ src/com/company/DistributionSuccessorFunction.java
@@ -11,6 +11,53 @@ public class DistributionSuccessorFunction implements SuccessorFunction {
         Estat board = (Estat) state;
 
 
+        //c es index camió
+        for (int c = 0; c < board.mida_camions(); ++c) {
+            //p es index de peticions
+            for (int p = 0; p < board.mida_peticions(); ++p) {
+                //si petició no atesa l'afegim
+                if (!board.peticio_atesa[c]) {
+                    //add petcició
+                    if (board.check_add(c, p)) {
+                        Estat new_state = new Estat(parametres de la creadora_Estat);
+                        new_state.add(c, p);
+                        retval.add(new Successor(new String("add C:" + c + " P" + p), new_state));
+                    }
+                }
+            }
+            //per cada viatge de camio
+            for (int v = 0; v < 5; ++v) {
+                //per cada petició de viatge
+                //la primera o la segona
+                for (int i = 0; i < 2; ++i) {
+
+                    //si no esta buida (es diferent a -1) procedim:
+                    if (board.GetCamioViatgePeticio(c, v, i)) {
+
+                        //mirem amb qui podem fer swap
+                        //per cada camio2
+                        for (int c2 = 0; c2 < board.mida_camions(); ++c2) {
+                            //per cada viatge de camio2
+                            for (int v2 = 0; v2 < 5; ++v2) {
+                                //per cada peticio de viatge
+                                //la primera o la segona
+                                for (int j = 0; j < 2; ++j) {
+                                    //podem fer swap:
+                                    if (board.check_swap(c, v, i, c2, v2, j)) {
+                                        Estat new_state = new Estat(parametres de la creadora_Estat);
+                                        new_state.swap(c, v, i, c2, v2, j);
+                                        retval.add(new Successor(new String("swap C: " + c + "  V: " + v + " " + i ";  C: " + c2 + "  V: " + v + " " + j), new_state));
+                                    }
+                                }
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
+
+
 
         return retval;
     }
